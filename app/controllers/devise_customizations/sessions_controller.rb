607dec0a9ebb2a1355958975bb86dd27ed679374
Jon Kinney <jonkinney@gmail.com>
2017-04-17 10:04:06 -0500

    Update sessions controller check for json/json_api

--- app/controllers/devise_customizations/sessions_controller.rb
+++ app/controllers/devise_customizations/sessions_controller.rb
@@ -1,29 +1,31 @@
 module DeviseCustomizations
   class SessionsController < Devise::SessionsController
-    skip_before_action :verify_authenticity_token, only: [:create, :destroy]
+    skip_before_action :verify_authenticity_token,
+                       only: [:create, :destroy],
+                       if: -> { json_request? }
 
     def create
-      if request.format == :json
-        user = warden.authenticate!(auth_options)
-        token = Tiddle.create_and_return_token(user, request)
-        render json: { authentication_token: token }
-      else
-        super
-      end
+      super and return unless json_request?
+
+      user = warden.authenticate!(auth_options)
+      token = Tiddle.create_and_return_token(user, request)
+      render json: { authentication_token: token }
     end
 
     def destroy
-      if request.format == :json
-        Tiddle.expire_token(current_user, request) if current_user
-        render json: {}
-      else
-        super
-      end
+      super and return unless json_request?
+
+      Tiddle.expire_token(current_user, request) if current_user
+      render json: {}
     end
 
     private
 
     # This is invoked before destroy and we have to override it
     def verify_signed_out_user; end
+
+    def json_request?
+      %i[api_json json].include?(request.format.to_sym)
+    end
   end
 end
