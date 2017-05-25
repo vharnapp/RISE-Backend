e3fdc4afd69a7fa6af8aaa8f1e1c4823ad9fd617
Jon Kinney <jonkinney@gmail.com>
2017-05-25 15:56:15 -0500

    Update paperclip to be local for dev

--- config/application.rb
+++ config/application.rb
@@ -44,14 +44,7 @@ module AthletefitBackend
     config.active_job.queue_adapter = :delayed_job
 
     config.paperclip_defaults = {
-      storage: :s3,
-      s3_region: ENV['AWS_REGION'],
-      s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
-      s3_credentials: {
-        bucket: ENV['S3_BUCKET_NAME'],
-        access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-      },
+      storage: :filesystem,
     }
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,6 +106,17 @@ Rails.application.configure do
   }
 
   config.action_mailer.default_url_options = { host: ENV.fetch("APPLICATION_HOST") }
+
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_region: ENV['AWS_REGION'],
+    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    },
+  }
 end
 
 Rack::Timeout.timeout = (ENV["RACK_TIMEOUT"] || 10).to_i
