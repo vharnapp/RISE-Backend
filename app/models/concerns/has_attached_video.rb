2ed854e82701122e8d8cf7291f6e2e5807dce1b8
Jon Kinney <jonkinney@gmail.com>
2017-06-02 15:47:49 -0500

    Missing per model

--- app/models/concerns/has_attached_video.rb
+++ app/models/concerns/has_attached_video.rb
@@ -3,7 +3,7 @@ module HasAttachedVideo
 
   included do
     has_attached_file :keyframe,
-                      default_url: '//s3-us-west-2.amazonaws.com/rise-media2/missing/:style/missing_keyframe.png',
+                      default_url: "//s3-us-west-2.amazonaws.com/rise-media2/missing_#{self.name.underscore.pluralize}/:style/missing_keyframe.png",
                       preserve_files: true,
                       styles: {
                         medium: '300x300>',
