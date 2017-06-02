463aded73082912f3f4fabb47a59049699825729
Jon Kinney <jonkinney@gmail.com>
2017-06-02 09:32:12 -0500

    Update phase serializer

--- app/resources/api/v1/pyramid_module_resource.rb
+++ app/resources/api/v1/pyramid_module_resource.rb
@@ -5,13 +5,18 @@ module Api
                  :description,
                  :level,
                  :prereq,
-                 :position,
                  :display_track,
                  :tracks,
                  :keyframe,
-                 :video
+                 :video,
+                 :position
 
       has_many :phases
+
+      # FIXME: (2017-06-02) jon => This isn't working, not 100% sure why
+      def self.default_sort
+        [{ field: :position, direction: :asc }]
+      end
     end
   end
 end
