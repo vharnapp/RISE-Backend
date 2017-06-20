b0850dc61606c335867a554094949beedfa4fdbd
Jon Kinney <jonkinney@gmail.com>
2017-06-20 12:57:44 -0500

    Add update and create for unlocked pyramid_modules

--- app/controllers/api/v1/unlocked_pyramid_modules_controller.rb
+++ app/controllers/api/v1/unlocked_pyramid_modules_controller.rb
@@ -1,12 +1,31 @@
 module Api
   module V1
     class UnlockedPyramidModulesController < BaseApiController
-      before_action :find_user, only: :index
-      load_resource only: :index
-      authorize_resource
+      before_action :find_user
+      load_and_authorize_resource
 
       def index
         jsonapi_render json: @user.unlocked_pyramid_modules
+      end
+
+      def create
+        unlocked_pyramid_module = UnlockedPyramidModule.new(resource_params)
+
+        if unlocked_pyramid_module.save
+          jsonapi_render json: unlocked_pyramid_module, status: :created
+        else
+          jsonapi_render_errors json: unlocked_pyramid_module,
+                                status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @unlocked_pyramid_module.update(resource_params)
+          jsonapi_render json: @unlocked_pyramid_module, status: :ok
+        else
+          jsonapi_render_errors json: @unlocked_pyramid_module,
+                                status: :unprocessable_entity
+        end
       end
 
       private
