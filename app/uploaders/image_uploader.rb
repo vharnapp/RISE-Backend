c141c7da2d1e738f85ba1e981b0f3a3ab9dcb7f5
Noah Settersten <noah@headway.io>
2017-06-27 11:42:30 -0500

    Remove default URL for user avatar

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,9 +15,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url
-    'http://plachold.it/300x300.png'
-  end
 end
