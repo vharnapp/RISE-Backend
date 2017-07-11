70605932ca36a7055587a1495393f081d6f19561
Jon Kinney <jonkinney@gmail.com>
2017-07-11 14:32:03 -0500

    logos and lifetime attempts

--- app/javascript/teams/show.js
+++ app/javascript/teams/show.js
@@ -4,9 +4,10 @@ import 'jquery-circle-progress';
 $(document).on('turbolinks:load', function() {
   $('.circle').circleProgress({
     startAngle: -Math.PI / 2,
-    size: '50',
+    size: '60',
     thickness: '3',
     animationStartValue: '0',
     fill: { color: "#96E17D" },
+    emptyFill: '#fff',
   });
 });
