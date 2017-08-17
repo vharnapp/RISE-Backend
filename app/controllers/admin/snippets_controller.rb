57e3abbe2d317708219000863bcdc5ad14fcc4d2
Jon Kinney <jonkinney@gmail.com>
2017-08-17 11:52:52 -0500

    Force re-generation of slug

--- app/controllers/admin/snippets_controller.rb
+++ app/controllers/admin/snippets_controller.rb
@@ -1,5 +1,10 @@
 module Admin
   class SnippetsController < Admin::ApplicationController
     include DefaultSort
+
+    def update
+      requested_resource.update_column(:slug, nil)
+      super
+    end
   end
 end
