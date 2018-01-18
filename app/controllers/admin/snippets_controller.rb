19584d0395a7420b1b70d8eb43cd8b719e4cb5ab
Jon Kinney <jonkinney@gmail.com>
2018-01-17 23:55:26 -0600

    Allow for creating new snippets from marketing site

--- app/controllers/admin/snippets_controller.rb
+++ app/controllers/admin/snippets_controller.rb
@@ -2,6 +2,18 @@ module Admin
   class SnippetsController < Admin::ApplicationController
     include DefaultSort
 
+    def edit
+      super
+    rescue ActiveRecord::RecordNotFound
+      redirect_to new_admin_snippet_path(name: params[:id])
+    end
+
+    def new
+      render locals: {
+        page: Administrate::Page::Form.new(dashboard, resource_class.new(name: params[:name].titleize)),
+      }
+    end
+
     def update
       requested_resource.update_column(:slug, nil)
       super
