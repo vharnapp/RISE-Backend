6c848a2b6298c1fae85a3784d24ff964bc7ec5d9
Jon Kinney <jonkinney@gmail.com>
2018-01-18 00:00:59 -0600

    Carry content forward on initial edit

--- app/controllers/admin/snippets_controller.rb
+++ app/controllers/admin/snippets_controller.rb
@@ -5,12 +5,12 @@ module Admin
     def edit
       super
     rescue ActiveRecord::RecordNotFound
-      redirect_to new_admin_snippet_path(name: params[:id])
+      redirect_to new_admin_snippet_path(name: params[:id], content: params[:content])
     end
 
     def new
       render locals: {
-        page: Administrate::Page::Form.new(dashboard, resource_class.new(name: params[:name].titleize)),
+        page: Administrate::Page::Form.new(dashboard, resource_class.new(name: params[:name].titleize, content: params[:content])),
       }
     end
 
