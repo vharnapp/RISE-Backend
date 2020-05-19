f8a17bca424cf9e6c84fab0404758256cdf53190
Zsolt Lukacs <zsolt@wesrom.com>
2020-05-19 16:03:21 +0300

    Issue of available modules for players after their team/club subscription is cancelled -- fixed

--- app/controllers/api/v1/unlocked_pyramid_modules_controller.rb
+++ app/controllers/api/v1/unlocked_pyramid_modules_controller.rb
@@ -4,10 +4,25 @@ module Api
       load_and_authorize_resource
 
       def index
-        unlocked_pyramid_modules =
-          current_user
-            .unlocked_pyramid_modules
-              .includes(:pyramid_module)
+        
+        # Check if the user is member of a team
+        # If yes, loads all his pyramid modules
+        # If Not, loads only the purhased ones, or the starter modules if he didn't purchased any of them
+        if current_user.subscriptions.merge(Subscription.current).present? || (current_user.subscription&.current? == true)
+          unlocked_pyramid_modules = current_user.unlocked_pyramid_modules.includes(:pyramid_module)
+        else 
+          pyramid_module_ids = PyramidModule.default_unlocked.pluck(:id)
+          single_payment_ids = current_user.archieved_user_payments.pluck(:single_payment_id)
+          
+          if single_payment_ids.count > 0
+            single_payments = SinglePayment.where(id: single_payment_ids)
+            single_payments.each do |item|
+              pyramid_module_ids = pyramid_module_ids + item.pyramid_modules.pluck(:id)
+            end
+          end
+
+          unlocked_pyramid_modules = current_user.unlocked_pyramid_modules.where(pyramid_module_id: pyramid_module_ids).includes(:pyramid_module)
+        end
 
         #if unlocked_pyramid_modules.blank? || (unlocked_pyramid_modules.present? && unlocked_pyramid_modules.map(&:pyramid_module_id) == [PyramidModule.find_by(level: 5).id])
         #  unlocked_pyramid_modules =
